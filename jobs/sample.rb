f287e1f51fd3879e3dc5de1872c5a0fb7f79ad2f
Grant Rutherford <grutherford@componentsense.com>
2013-04-08 12:32:17 +0100

    updates

--- jobs/sample.rb
+++ jobs/sample.rb
@@ -11,18 +11,18 @@ SCHEDULER.every '2s' do
   current_valuation = rand(100)
   current_karma     = rand(200000)
   
-  if last_orders = 10
+  if last_orders == 10
   	current_orders = 0
   else
   	current_orders = last_orders+1
   end
 
-  if last_quotes = 100
+  if last_quotes == 100
   	current_quotes = 0
   else 
 	current_quotes = last_quotes+5
   end
-  
+
   send_event('valuation', { current: current_valuation, last: last_valuation })
   send_event('karma', { current: current_karma, last: last_karma })
   send_event('quotes',   { value: current_quotes })
