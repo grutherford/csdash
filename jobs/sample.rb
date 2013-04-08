04a9da3766c79be4ffc7459cbdd06a48d51279d5
Grant Rutherford <grutherford@componentsense.com>
2013-04-08 12:14:07 +0100

    update to limit order/quote job

--- jobs/sample.rb
+++ jobs/sample.rb
@@ -10,10 +10,17 @@ SCHEDULER.every '2s' do
   last_orders	 = current_orders
   current_valuation = rand(100)
   current_karma     = rand(200000)
-  current_orders = last_orders+1
-  current_quotes = last_quotes+5
-
-
+  
+  if last_orders = 10
+  	current_orders = 0
+  else
+  	current_orders = last_orders+1
+  
+  if current_quotes = 100
+  	current_quotes = 0
+  else 
+	current_quotes = last_quotes+5
+  
   send_event('valuation', { current: current_valuation, last: last_valuation })
   send_event('karma', { current: current_karma, last: last_karma })
   send_event('quotes',   { value: current_quotes })
